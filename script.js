ad9f8acbbf9398faef007e6c17202892190dc09b
namealperalp <namealperalp@gmail.com>
2024-06-11 11:39:27 +0300

    Yeni bir bakış:-)

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
-function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+// Tab İşlevselliği
+function openPage(pageName, element, color) {
+  // Tüm tab içeriklerini gizle
+  const tabContents = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabContents.length; i++) {
+      tabContents[i].style.display = "none";
   }
 
-  // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
+  // Tüm tab butonlarının arka plan rengini sıfırla
+  const tabLinks = document.getElementsByClassName("tablink");
+  for (let i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].style.backgroundColor = "";
   }
 
-  // Show the specific tab content
+  // Seçilen tab içeriğini göster
   document.getElementById(pageName).style.display = "block";
 
-  // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
+  // Seçilen tab butonunun arka plan rengini ayarla
+  element.style.backgroundColor = color;
 }
 
-// Get the element with id="defaultOpen" and click on it
-
+// Sayfa yüklendiğinde varsayılan olarak "Neden Biz?" tabını aç
 window.onload = function() {
-document.getElementById("defaultOpen").click();
-}
+  document.getElementById("defaultOpen").click();
+};
+
+  
+  
+
+
+// İleride eklenebilecek diğer JavaScript fonksiyonları (örneğin form doğrulama, animasyonlar vb.)
